3f8adbf7f4f779eab87869eba575b3da008a28ad
Jose Septien <jaseptienh@gmail.com>
2019-04-02 16:54:46 -0600

    Add functionality for drawing the scenario with obstacles and roadmap.

--- pOccGrid.py
+++ pOccGrid.py
@@ -7,6 +7,8 @@ Created on Tue Mar 19 17:08:08 2019
 """
 import numpy as np
 import skimage.draw as skd
+import matplotlib.pyplot as plt
+import math as m
 
 class OccupancyGrid:
     def __init__(self, row, col):
@@ -29,6 +31,7 @@ class OccupancyGrid:
         """
         for i in range(len(sState)):
             line = self.getSightLine(sPosition[i], sPoints[i])
+            rows, cols = line[0], line[1]
             # Get the previous probability the cell
             pm = self.occGrid[rows[-1], cols[-1]]
             pnm = 1 - pm
@@ -43,3 +46,56 @@ class OccupancyGrid:
                 pm = self.occGrid[rows[i], cols[i]]
                 pnm = 1 - pm
                 self.occGrid[rows[i], cols[i]] = (self.pnzm * pm) / ((self.pnzm * pm) + (self.pnznm * pnm))
+
+    def plotGrid(self):
+        plt.imshow(self.occGrid, "viridis")
+        plt.show()
+ 
+    def addObjects(self, obstacles, epsilon, h, w, s):
+        """ Adds circles to the occupancy grid. obstacles is an array of circles """
+        def getGridCoord(xw, yw, h, w, s):
+            """ Given a world coordinate, it returns it index on the grid """
+            row = int(h / (2 * s) - m.floor(yw / s))
+            col = int(m.ceil(xw / s) + w / (2 * s))
+            return row, col
+        #
+        for obj in obstacles:
+            row, col = getGridCoord(obj[0], obj[1], h, w, s)
+            r = obj[2] / s
+            e = epsilon / s
+            rr, cc = skd.circle(row, col, r - e)
+            self.occGrid[rr, cc] = 0.8
+            rr, cc = skd.circle(row, col, r)
+            self.occGrid[rr, cc] = 1
+            
+    def addPoints(self, points, edges, h, w, s, color):
+        """ """
+        def getGridCoord(xw, yw, h, w, s):
+            """ Given a world coordinate, it returns it index on the grid """
+            row = int(h / (2 * s) - m.floor(yw / s))
+            col = int(m.ceil(xw / s) + w / (2 * s))
+            return row, col
+        #
+        for i in range(len(points)):
+            row, col = getGridCoord(points[i][0], points[i][1], h, w, s)
+            rr, cc = skd.circle(row, col, 2)
+            self.occGrid[rr, cc] = color
+            for e in edges[i]:
+                row1, col1 = getGridCoord(points[e][0], points[e][1], h, w, s)
+                rr, cc = skd.line(row, col, row1, col1)
+                self.occGrid[rr, cc] = color
+                
+    def addRoute(self, route, h, w, s):
+        """ """
+        def getGridCoord(xw, yw, h, w, s):
+            """ Given a world coordinate, it returns it index on the grid """
+            row = int(h / (2 * s) - m.floor(yw / s))
+            col = int(m.ceil(xw / s) + w / (2 * s))
+            return row, col
+        for i in range(len(route) - 1):
+            pI = route[i]
+            pF = route[i + 1]
+            row, col = getGridCoord(pI[0], pI[1], h, w, s)
+            row1, col1 = getGridCoord(pF[0], pF[1], h, w, s)
+            rr, cc = skd.line(row, col, row1, col1)
+            self.occGrid[rr, cc] = 0.5
