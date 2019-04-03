383c11a72745a1629ba6f1ec01bf3fc75cbfd554
Jose Septien <jaseptienh@gmail.com>
2019-04-03 15:36:12 -0600

    Modify priority queue to use node class.

--- PQueue.py
+++ PQueue.py
@@ -5,6 +5,7 @@ A priority queue
 @author: Phantom
 """
 import math as m
+import node as n
 
 class PQueue:
     def __init__(self):
@@ -19,6 +20,7 @@ class PQueue:
             return
         minE = self.queue[0]
         self.queue[0] = self.queue[-1]
+        self.queue.pop(-1)
         self.len -= 1
         self.min_heapify(0)
         return minE
@@ -26,35 +28,37 @@ class PQueue:
     def min_heapify(self, i):
         if self.len == 0:
             return
-        l  = 2 * i
-        r = (2 * i) + 1
-        if l <= self.len and self.queue[l][1] > self.queue[r][1]:
-            largest = l
+        l  = (2 * i) + 1
+        r = 2 * (i + 1)
+        if l < self.len and self.queue[l][1].d < self.queue[i][1].d:
+            least = l
         else:
-            largest = r
-        if r <= self.len and self.queue[r][1] > self.queue[largest][1]:
-            largest = r
-        if largest != i:
+            least = i
+        if r < self.len and self.queue[r][1].d < self.queue[least][1].d:
+            least = r
+        if least != i:
             aux = self.queue[i]
-            self.queue[i] = self.queue[largest]
-            self.queue[largest] = aux
-            self.min_heapify(largest)
+            self.queue[i] = self.queue[least]
+            self.queue[least] = aux
+            self.min_heapify(least)
             
     def decrease_key(self, i, key):
         def parent(i):
-            return int(i / 2)
-        if key[1] > self.queue[i][1]:
+            return int((i - 1) / 2)
+        if key[1].d > self.queue[i][1].d:
             return
         self.queue[i] = key
-        while i > 0 and self.queue[parent(i)][1] < self.queue[i][1]:
+        p = parent(i)
+        while i > 0 and self.queue[p][1].d > self.queue[i][1].d:
             aux = self.queue[i]
             self.queue[i] = self.queue[parent(i)]
             self.queue[parent(i)] = aux
             i = parent(i)
+            p = parent(i)
 
     def insert_key(self, key):
         self.len += 1
-        self.queue.append((-1,m.inf))
+        self.queue.append((-1,n.Node((0, 0))))
         self.decrease_key(self.len - 1, key)
     
     def isEmpty(self):
